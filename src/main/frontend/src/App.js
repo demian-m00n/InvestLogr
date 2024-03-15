28cf9c9938d2018ed0d7374d35df8990f19f5545
demian-m00n <m00n.seolmin@gmail.com>
2024-03-15 21:27:19 +0900

    FEAT: FRONTEND LOGIN PAGE 구현

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,20 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import Button from '@mui/material/Button'
+import Login from './components/Login'
+
 
 function App() {
-   const [hello, setHello] = useState('')
 
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+    return(
+        <div className="App">
         </div>
-    );
+    )
 }
 
 export default App;
